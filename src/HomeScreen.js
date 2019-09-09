47d6ba42ec07a34d728897fce19bea2c1a9b21a1
chetta chetta chetta <>
2019-09-09 14:01:34 +0700

    add background

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
 
 export default class HomeScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
 
-  render() {
-    return (
-      <View>
-        <Text> HomeScreen </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <ImageBackground
+                source={require("./assets/img/bg.png")}
+                style={{
+                    flex: 1,
+                    flexDirection: "column"
+                }}>
+                <View>
+                    <Text> HomeScreen </Text>
+                </View>
+            </ImageBackground>
+        );
+    }
 }
