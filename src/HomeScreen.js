24026f56140e5f5721697b670b29519cba3ac727
chetta chetta chetta <>
2019-09-09 14:35:00 +0700

    add codestructor login page

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,23 +1,69 @@
 import React, { Component } from 'react';
-import { View, Text, ImageBackground } from 'react-native';
+import { Text, View, ImageBackground, TextInput, Button, TouchableOpacity } from 'react-native';
 
 export default class HomeScreen extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
-    }
-
     render() {
         return (
             <ImageBackground
-                source={require("./assets/img/bg.png")}
-                style={{
-                    flex: 1,
-                    flexDirection: "column"
-                }}>
-                <View>
-                    <Text> HomeScreen </Text>
+                source={require('./assets/img/bg.png')}
+                style={{ flex: 1, flexDirection: 'column' }}>
+                {/* Login section */}
+                <View
+                    style={{
+                        backgroundColor: '#FFF3',
+                        marginTop: 16,
+                        marginLeft: 16,
+                        marginRight: 16,
+                        paddingTop: 16,
+                        paddingLeft: 16,
+                        paddingRight: 16,
+                        borderRadius: 5,
+                    }}>
+                    {/* Username section */}
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <View style={{
+                            height: 40,
+                            width: 40,
+                            backgroundColor: 'red',
+                            borderRadius: 20,
+                            marginRight: 8
+                        }} />
+                        <TextInput style={{
+                            flex: 1,
+                            height: 40,
+                            borderRadius: 3,
+                            borderWidth: 1,
+                            borderColor: "#0003",
+                            paddingLeft: 8
+                        }}
+                            placeholder="Username" />
+                    </View>
+
+                    {/* Password section */}
+                    <View style={{ marginTop: 8, flexDirection: 'row', alignItems: 'center' }}>
+                        <View style={{
+                            height: 40,
+                            width: 40,
+                            backgroundColor: 'yellow',
+                            borderRadius: 20,
+                            marginRight: 8
+                        }} />
+                        <TextInput style={{
+                            flex: 1,
+                            height: 40,
+                            borderRadius: 3,
+                            borderWidth: 1,
+                            borderColor: "#0003",
+                            paddingLeft: 8
+                        }}
+                            placeholder="Password" />
+                    </View>
+                    <View style={{ marginTop: 16 }}>
+                        <Button title="Login" />
+                    </View>
+                    <TouchableOpacity style={{ marginTop: 8, marginBottom: 16, backgroundColor: "#fff4", height: 40, borderTopEndRadius:5, justifyContent: "center" }}>
+                        <Text style={{ textAlign: 'center', color: "#0009" }}>Register</Text>
+                    </TouchableOpacity>
                 </View>
             </ImageBackground>
         );
